c16b541666c90be756965d6f1ad7994f83f8133e
IoanGrozniy <126981808+IoanGrozniy228@users.noreply.github.com>
2026-09-16 07:21:42 +0000

    added layout

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,14 @@ import ContactsPage from './pages/ContactsPage.jsx';
 import NotFoundPage from './pages/NotFoundPage.jsx';
 import { products, categories } from './data/products.js';
 import { Route, Routes } from 'react-router-dom';
+import Layout from './components/Layout.jsx';
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<HomePage/>} />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+      </Route>
     </Routes>
   )
 }
